01fbfc5615012350bc7dcf919ea7767a2f464853
voitovmichael <voitovmichael@yandex.ru>
2021-10-23 13:19:18 +0300

    feat: Настроить валидацию

--- middlewears/auth.js
+++ middlewears/auth.js
@@ -3,20 +3,15 @@ const jwt = require('jsonwebtoken');
 const IncorectAuth = require('../errors/incorect-auth');
 
 const auth = (req, res, next) => {
-  // console.log('auth');
   const { token } = req.headers;
   if (!token) {
     next(new IncorectAuth('Необходима авторизация'));
-    // console.log('auth');
-    // res.status(401).send({ message: 'Необходима авторизация' });
   } else {
     let payload;
     try {
       payload = jwt.verify(token, 'some-secret-key');
     } catch (err) {
-      // console.log('auth');
       next(new IncorectAuth('Необходима авторизация'));
-      // res.status(401).send({ message: 'Необходима авторизация' });
     }
     req.user = payload;
     next();
