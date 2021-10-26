e0199102cf0747dc4a8257d94f847c030a15fe16
voitovmichael <voitovmichael@yandex.ru>
2021-10-26 22:30:07 +0300

    fix: Исправить ошибки третья итерация

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
-const NotFound = require('../errors/not-found-err');
 const IncorectAuth = require('../errors/incorect-auth');
 
 const userSchema = new mongoose.Schema({
